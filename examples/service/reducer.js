29f423932b2ccc52bc1e22685ee31f8945a07b0a
Álvaro Cuesta <alvaro@redradix.com>
2020-09-07 09:41:21 +0200

    Rename back to Redukers (name with K was taken)

--- examples/service/reducer.js
+++ examples/service/reducer.js
@@ -1,20 +1,20 @@
 import { combineReducers } from 'redux'
-import * as reduckers from '../../lib'
+import * as redukers from '../../lib'
 import * as actionTypes from './action-types'
 
 export default combineReducers({
-  foo: reduckers.value({
+  foo: redukers.value({
     set: actionTypes.SET_FOO,
      // Notice how these are shared with fooRequest and bar.reset
     reset: [actionTypes.FETCH_FOO, actionTypes.ERROR_FOO, actionTypes.RESET_ALL],
   }),
 
-  bar: reduckers.value({
+  bar: redukers.value({
     set: actionTypes.SET_BAR,
     reset: actionTypes.RESET_ALL,
   }, 1337),
 
-  fooRequest: reduckers.request({
+  fooRequest: redukers.request({
     start: actionTypes.FETCH_FOO,
     success: actionTypes.SET_FOO, // Notice how this is shared with foo.set
     error: actionTypes.ERROR_FOO,
