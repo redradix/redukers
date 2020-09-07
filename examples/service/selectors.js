b2f71489703f75141613053e487de0214b5fe94e
Andres Martin <monkeyandres@protonmail.com>
2020-09-07 09:59:25 +0200

    Move pipe to fp-utils

--- examples/service/selectors.js
+++ examples/service/selectors.js
@@ -1,5 +1,5 @@
 import request from '../../lib/request'
-import { pipe } from '../../lib/utils'
+import * as F from '../../lib/fp-utils'
 
 export const getFoo = state => state.foo
 
@@ -7,32 +7,32 @@ export const getBar = state => state.bar
 
 const getFooRequest = state => state.fooRequest
 
-export const getIsEmptyFoo = pipe(
+export const getIsEmptyFoo = F.pipe(
   getFooRequest,
   request.getIsEmpty,
 )
 
-export const getIsRequestingFoo = pipe(
+export const getIsRequestingFoo = F.pipe(
   getFooRequest,
   request.getIsRequesting,
 )
 
-export const getIsRequestSucceededFoo = pipe(
+export const getIsRequestSucceededFoo = F.pipe(
   getFooRequest,
   request.getIsRequestSucceeded,
 )
 
-export const getIsErrorFoo = pipe(
+export const getIsErrorFoo = F.pipe(
   getFooRequest,
   request.getIsError,
 )
 
-export const getErrorFoo = pipe(
+export const getErrorFoo = F.pipe(
   getFooRequest,
   request.getError,
 )
 
-export const getIsLoadingFoo = pipe(
+export const getIsLoadingFoo = F.pipe(
   getFooRequest,
   request.getIsLoading,
 )
